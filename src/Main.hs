cb1c0f13b5a0cb02b9d71f3979792c0bbe983203
Catherine Galkina <anais94@yandex.ru>
2015-03-25 19:45:34 +0300

    Init the universe

--- src/Main.hs
+++ src/Main.hs
@@ -2,12 +2,27 @@ module Main where
 
 import Control.Exception
 
-data Cell = Dead | Alive
+data Cell = Dead | Alive deriving (Show)
 
-data Universe = Universe [[Cell]]
+data Universe = Universe [[Cell]] deriving (Show)
 
---initState :: [(Int, Int)] -> Universe
+initUni :: [(Int, Int)] -> Universe
+initUni l =  Universe . reverse . map reverse $ 
+    if length l >= 1 then initState x y (tail l) else defState where
+                                                         x = fst (head l) - 1
+                                                         y = snd (head l) - 1
 
+defState = [[]]
+
+initState :: Int -> Int -> [(Int, Int)] -> [[Cell]]
+initState (-1) _ _ = []
+initState x y l = (initRow x y l) : (initState (x-1) y l)
+
+initRow :: Int -> Int -> [(Int, Int)] -> [Cell]
+initRow _ (-1) _ = []
+initRow x y l
+        | elem (x, y) l = Alive : initRow x (y-1) l
+        | otherwise = Dead : initRow x (y-1) l
 --run :: Universe -> IO()
 
 --update :: Universe -> Universe
@@ -28,4 +43,4 @@ main = do
     result <- try $ readFile config :: IO (Either IOException String)
     case result of
       Left exception -> putStrLn $ "Error: " ++ show exception
-      Right content -> putStrLn . show . positions $ content
+      Right content -> putStrLn . show . initUni . positions $ content
