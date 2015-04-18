2d625a79a6c8ed6791fd55440ecfc6bce3a37858
Catherine Galkina <anais94@yandex.ru>
2015-04-18 22:21:29 +0400

    Some fuctions for update

--- src/Game.hs
+++ src/Game.hs
@@ -8,4 +8,30 @@ type Universe = (Matrix Cell)
 
 --run :: Universe -> IO ()
 
---update :: Universe -> Universe
+update :: Universe -> Universe
+update u = updateRow u 1 (matrix (nrows u) (ncols u) (\(i,j) -> Dead))
+
+updateRow :: Universe -> Int -> Universe -> Universe
+updateRow old r new | r == (nrows old) + 1 = new
+                    | otherwise = updateRow old (r+1) (updateCol old r 1 new)
+
+updateCol :: Universe -> Int -> Int -> Universe -> Universe
+updateCol old r c new | c == (ncols old) + 1 = new
+                      | otherwise = if (border r c) then 
+                                        updateBorder old r c new
+                                    else updateCommon old r c new
+
+updateBorder :: Universe -> Int -> Int -> Universe -> Universe
+updateBorder = undefined
+
+updateCommon :: Universe -> Int -> Int -> Universe -> Universe
+updateCommon = undefined
+
+isAlive :: Cell -> Int
+isAlive Alive = 1
+isAlive _ = 0
+
+newState :: Cell -> Int -> Cell
+newState Alive 2 = Alive
+newState _ 3 = Alive
+newState _ _ = Dead
