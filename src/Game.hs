9a80fdfd8e3cb826f9b30aa9dcd03ca5a0a86f88
Catherine Galkina <anais94@yandex.ru>
2015-04-21 00:19:24 +0400

    Comments and codestyle

--- src/Game.hs
+++ src/Game.hs
@@ -5,10 +5,16 @@ import Graphics.Gloss
 
 data Cell = Dead | Alive deriving (Show)
 
-cellSize = 24
-
 type Universe = (Matrix Cell)
 
+cellSize = 24
+
+--Main function for running the automata
+run :: Universe -> IO ()
+run universe = play (InWindow "Conway`s Life" (windowSize universe) (240, 160)) 
+               white 2 universe renderer handler updater
+
+--Create pictures of dead (empty rectangle) or alive (solid rectangle) cells
 drawCell :: Float -> Float -> Cell -> Picture
 drawCell x y cell = let figure = case cell of
                                  Dead -> rectangleWire
@@ -17,17 +23,16 @@ drawCell x y cell = let figure = case cell of
                                  (cellSize / 2 + cellSize * y)
                                  (figure cellSize cellSize)
 
-run :: Universe -> IO ()
-run universe = play (InWindow "Conway`s Life" (windowSize universe) (240, 160)) 
-               white 2 universe renderer handler updater
-
+--Calculate window size for current universe size
 windowSize :: Universe -> (Int, Int)
 windowSize u = ((round cellSize) * (nrows u), (round cellSize) * (ncols u))
 
+--Check if the cell is alive
 isAlive :: Cell -> Bool
 isAlive Alive = True
 isAlive Dead = False
 
+--Get moore neighbourhood of a cell as a list of neighbouring cells
 getNeighbours :: Universe -> Int -> Int -> [Cell]
 getNeighbours u x y = [u ! (i, j) | i <- [x - 1 .. x + 1], 
                                     j <- [y - 1 .. y + 1],
@@ -35,37 +40,54 @@ getNeighbours u x y = [u ! (i, j) | i <- [x - 1 .. x + 1],
                                     j >= 1, j <= (nrows u),
                                     i /= x || j /= y]
 
+--Updates the cell according to the number of its neighbours
 stepCellAux :: Cell -> [Cell] -> Cell
-stepCellAux cell neighbours = let live = length (filter isAlive neighbours)
-                           in case cell of
-                               Dead -> if live == 3 then Alive else Dead
-                               Alive -> if live == 2 || live == 3 then Alive else Dead
+stepCellAux cell neighbours = let live = length (filter isAlive neighbours) in 
+                                case cell of
+                                    Dead -> if live == 3 then 
+                                                Alive 
+                                            else    
+                                                Dead
+                                    Alive -> if live == 2 || live == 3 then 
+                                                Alive 
+                                             else 
+                                                Dead
 
+--Update function for each cell
 stepCell :: Universe -> Int -> Int -> Cell
 stepCell u x y = stepCellAux (u ! (x, y)) (getNeighbours u x y)
 
+--Auxillary function to update the universe cell by cell
 stepUniverseAux :: Universe -> Universe -> Int -> Int -> Universe
 stepUniverseAux oldu newu 1 1 = setElem (stepCell oldu 1 1) (1, 1) newu
-stepUniverseAux oldu newu x 1 = stepUniverseAux oldu 
-                                                (setElem (stepCell oldu x 1) (x, 1) newu)
-                                                (x - 1) (ncols oldu)
-stepUniverseAux oldu newu x y = stepUniverseAux oldu 
-                                                (setElem (stepCell oldu x y) (x, y) newu)
-                                                x (y - 1)
+stepUniverseAux oldu newu x 1 = stepUniverseAux 
+                                       oldu 
+                                       (setElem (stepCell oldu x 1) (x, 1) newu)
+                                       (x - 1) (ncols oldu)
+stepUniverseAux oldu newu x y = stepUniverseAux 
+                                       oldu 
+                                       (setElem (stepCell oldu x y) (x, y) newu)
+                                       x (y - 1)
 
+--Universe updater
 stepUniverse :: Universe -> Universe
 stepUniverse u = stepUniverseAux u u (nrows u) (ncols u)
 
+--Update the universe on each step
 updater :: Float -> Universe -> Universe
 updater _ u = stepUniverse u
 
---handler :: Event -> Universe -> Universe
+--Event handler function which doesn't change the universe for now
 handler _ = id
 
+--Create a picture as a list (superposition) of cells 
 drawUniverse :: Universe -> Picture
-drawUniverse u = pictures [drawCell (fromIntegral (x - 1)) (fromIntegral (y - 1)) 
-             (u ! (x, y)) | x <- [1 .. (ncols u)], y <- [1 .. (nrows u)]]
+drawUniverse u = pictures [drawCell 
+                           (fromIntegral (x - 1)) (fromIntegral (y - 1)) 
+                           (u ! (x, y)) 
+                           | x <- [1 .. (ncols u)], y <- [1 .. (nrows u)]]
 
+--Render a picture for each step
 renderer :: Universe -> Picture
 renderer u = let (windowWidth, windowHeight) = windowSize u
                  offsetX = - fromIntegral windowWidth / 2
--- src/Init.hs
+++ src/Init.hs
@@ -5,6 +5,7 @@ import Data.Maybe
 import Data.Matrix
 import Game
 
+--Create a matrix of dead/alive cells
 initUni :: [(Int, Int)] -> Universe
 initUni l = if length l > 1 then 
                 transpose . fromLists . reverse . map reverse $ 
@@ -13,36 +14,45 @@ initUni l = if length l > 1 then
                             x = fst (head l) - 1
                             y = snd (head l) - 1
 
+--Default state in case of empty config file
 defState :: Universe
 defState = matrix 20 20 glider
 
+--Create one glider in lower left corner
 glider :: (Int, Int) -> Cell
 glider (i, j) | (i == 2) && (j == 1) || 
                 (i == 3) && (j == 2) ||
                 (i <= 3) && (j == 3) = Alive
               | otherwise = Dead
 
+--Make list of lists of cells to be later transformed into matrix
 initState :: Int -> Int -> [(Int, Int)] -> [[Cell]]
 initState (-1) _ _ = []
 initState x y l = (initRow x y l) : (initState (x-1) y l)
 
+--Make row of cells
 initRow :: Int -> Int -> [(Int, Int)] -> [Cell]
 initRow _ (-1) _ = []
 initRow x y l
         | elem (x+1, y+1) l = Alive : initRow x (y-1) l
         | otherwise = Dead : initRow x (y-1) l
 
+--Get positions of alive cells
 positions :: String -> [(Int, Int)]
 positions = getInts . (map words) . lines
 
+--Check input and transform coordinates into (Int, Int) if possible
 getInts :: [[String]] -> [(Int, Int)]
 getInts = tuples .  checkInput . map (map readMaybe)
 
+--Filters wrong config lines
 checkInput :: [[Maybe Int]] -> [[Int]]
 checkInput = map (map fromJust) . filter onlyJust
 
+--Checks if list has only (Just Int) elements
 onlyJust :: [Maybe Int] -> Bool
 onlyJust = and . map isJust
 
+--Convert lists of 2 Ints into tuples
 tuples :: [[Int]] -> [(Int, Int)]
 tuples = foldr (\x acc -> if length x == 2 then (x!!0, x!!1) : acc else acc) []
